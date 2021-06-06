0d4b29a3f85c24712b8f54dbb5d1811fa0471546
Berat Ceylan <Beratcey51@gmail.com>
2021-06-06 18:27:53 +0300

    Ders Tekrarı

--- Odevler/jQueryOdevi/cozum1.js
+++ Odevler/jQueryOdevi/cozum1.js
@@ -1,3 +1,20 @@
+// görev 1
+$("#gorev1").click(function () // id'si gorev1 olan etikete tıklandığında belirtilen fonksiyonu çalıştır
+{
+    $("h2").slice(0, 3).each(function () // h2 etiketlerinden ilk 3'ünü gez 
+    {
+        var metin = $(this).text(); // o anki etiketin içeriğini metin değişkenine ata
+        $("<li/>").text(metin).appendTo("#gorev1 > ul"); // li etiketi oluştur. içeriğine metin değişkenini yaz id'si gorev1 olan etiketin içerisindeki ul etiketine ekle
+    });
+});
+
+// görev 2
+$("#gorev2").click(function() // id'si gorev2 olan etikete tıklandığında belirtilen fonksiyonu çalıştır
+{
+    var sayi = $("h2").length; // h2 etiketlerinin eleman sayısını sayi değişkenine ata
+    $("#gorev2 > input:text").val(sayi); // id'si gorev2 olan etiketin tipi text olanların değerine sayi değişkenini ver
+});
+
 // gorev 3
 $("#gorev3").click(function () // id'si gorev3 olan etikete tıklandığında fonksiyon çalıştır
 {
@@ -8,7 +25,7 @@ $("#gorev3").click(function () // id'si gorev3 olan etikete tıklandığında fo
 // görev 4
 $("#gorev4").click(function () // id'si gorev4 olan etikete tıklandığında fonksiyon çalıştır
 {
-  $("p").each(function (index, element) // p etiketlerinin herbirini gez fonksiyonu çalıştır.
+  $("p").each(function () // p etiketlerinin herbirini gez fonksiyonu çalıştır.
   {
     var metin = $(this).text(); // p etiketlerinin içerisindeki metinleri metin değişkenine atadık
     $(this) // o anki p etiketi
